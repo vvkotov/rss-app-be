bc2a1b5ee9ddafd773b69d24c44d2f515565d5c9
Vitaly kotov <vitaly.kotov@bawagpsk.com>
2020-11-01 00:32:32 +0300

    create product service

---
+++ product-service/serverless.ts
@@ -0,0 +1,65 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'product-service',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    getProducts: {
+      handler: 'handler.getProductsList',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'products',
+            cors: true
+          }
+        }
+      ]
+    },
+    getProductById: {
+      handler: 'handler.getProductById',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'products/{productId}',
+            cors: true,
+            request: {
+              parameters: {
+                paths: {
+                  productId: true
+                }
+              }
+            } 
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
